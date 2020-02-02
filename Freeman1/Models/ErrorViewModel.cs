57d7ba4d783390673bc9dd47efbb68cfdd761280
Roomey12 <54270364+Roomey12@users.noreply.github.com>
2020-02-02 19:25:44 +0200

    first commit

---
+++ Freeman1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Freeman1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
